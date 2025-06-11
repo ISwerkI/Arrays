cc4f92d42a0142cd0683563455c914180d8ef0a0
Danil <Danil@DESKTOP-99KGHPD>
2025-06-11 18:33:36 +1000

    HEX DONE

--- Сonversion/Сonversion.cpp
+++ Сonversion/Сonversion.cpp
@@ -72,5 +72,19 @@ void main()
 		}
 	}
 #endif
+	int decimal = 200;
+	const int MAX_HEX_CAPACITY = 8; 
+	char hex[MAX_HEX_CAPACITY] = {};
+	int i = 0; 
+	for (; decimal; decimal /= 16)
+	{
+		hex[i++] = decimal % 16 < 10 ? decimal % 16 + 48 : decimal % 16 +55;
+		//hex[i] = decimal % 16; 
+		// //hex[i++] += hex[i] < 10 ? 48 : 55; 
+	}
+		for (--i; i >= 0; i--)
+			cout << hex[i];
+		//for (--i; i >= 0; i--)cout << char(hex[i] < 10 ? hex[i] + 48 
+		cout << endl;
 
 }
