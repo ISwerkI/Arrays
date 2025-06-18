aa6177327a7e5f1df6249fbbdca82c2502c8dee6
Danil <Danil@DESKTOP-99KGHPD>
2025-06-18 17:12:52 +1000

    Hw arr2 18.06

--- Arr2DShift/Arr2DShift.cpp
+++ Arr2DShift/Arr2DShift.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 #define delimeter "\n----------------------------------------\n"
+//#define RIGHT_SHIFT
+//#define LEFT_SHIFT
 void main()
 {
-    setlocale(LC_ALL, "");
+	setlocale(LC_ALL, "");
 	const int ROWS = 3;
 	const int COLS = 10;
 	int shift;
@@ -12,7 +14,7 @@ void main()
 	int arr[ROWS][COLS] =
 	{
 		{0,1,2,3,4,5,6,7,8,9},
-		{10,12,13,14,15,16,17,18,19},
+		{10,11,12,13,14,15,16,17,18,19},
 		{20,21,22,23,24,25,26,27,28,29},
 	};
 	for (int i = 0; i < ROWS; i++)
@@ -46,8 +48,8 @@ void main()
 		cout << endl;
 	}
 #endif
-#ifdef SHIFT
-	for (int i = 0; i < shift; i++)
+#ifdef LEFT_SHIFT
+	for (int b = 0; b < shift; b++ )
 	{
 		int buffer = arr[0][0];
 		for (int i = 0; i < ROWS; i++)
@@ -65,6 +67,29 @@ void main()
 		for (int j = 0; j < COLS; j++)
 		{
 			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+#endif
+#ifdef RIGHT_SHIFT
+	for (int b = 0; b < shift; b++)
+	{
+		int buffer = arr[ROWS - 1][COLS - 1];
+		for (int i = ROWS - 1; i >= 0; i--)
+		{
+			for (int j = COLS - 1; j > 0 ; j--)
+			{
+				arr[i][j] = arr[i][j - 1];
+			}
+			if (i != 0) arr[i][0] = arr[i - 1][COLS - 1];
+			else arr[i][0] = buffer;
+		}
+	}
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << "\t";                  
 		}
 		cout << endl;
 	}
--- Search2/Search2.cpp
+++ Search2/Search2.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main()
+void main()
 {
 	setlocale(LC_ALL, "");
 	const int ROWS = 4;
 	const int COLS = 5;
-	int max_num = 16, min = 0;
+	int max_num = 15, min = 0;
 	int arr[ROWS][COLS];
+	int used[ROWS * COLS];
+	int counter;
+	bool check = 0;
 	for (int i = 0; i < ROWS; i++)
 	{
 		for (int b = 0; b < COLS; b++)
@@ -15,8 +18,6 @@ int main()
 			arr[i][b] = rand() % (max_num - min + 1) + min;
 		}
 	}
-	int unic;
-	int used[ROWS * COLS];
 	for (int i = 0; i < ROWS; i++)
 	{
 		for (int j = 0; j < COLS; j++)
@@ -26,43 +27,32 @@ int main()
 		cout << endl;
 	}
 	cout << endl;
+
 	for (int i = 0; i < ROWS; i++)
 	{
 		for (int j = 0; j < COLS; j++)
 		{
-			for (int b = 0; b < COLS * ROWS;b++)
+			counter = 1;
+			for (int a = 0; a < ROWS; a++)
+			{
+				for (int b = ; b < COLS; b++)
+				{
+					if (arr[i][j] == arr[a][b])counter++;
+				}
+			}
+			for (int b = 0; b < ROWS * COLS; b++)
 			{
 				if (arr[i][j] == used[b])
 				{
-					for (int a = 0; a < min * max_num; a++)
-					{
-						for (int g = 0; g < COLS * ROWS; a++)
-						{
-							if (a == used[g])
-							{
-								int unic;
-								break;
-							}
-							else
-							{
-								unic = a;
-							}
-						}
-					}
-					arr[i][j] = unic;
+					check = 1;
+					break;
 				}
-				used[i+j] = arr[i][j];
+			}
+			if (counter > 1 and check == 0)
+			{
+				used[i + j] = arr[i][j];
+				cout << arr[i][j] << " в массиве повторяется " << counter << " раз" << endl;
 			}
 		}
 	}
-
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			cout << arr[i][j] << "\t";
-		}
-		cout << endl;
-	}
-
 }
